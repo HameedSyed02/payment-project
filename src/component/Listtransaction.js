531c1069ec4bddf1ef36287d3a31df4940e6cbf9
Hameed <Hameed@gmail.com>
2022-08-09 07:54:50 +0000

    commit

--- src/component/Listtransaction.js
+++ src/component/Listtransaction.js
@@ -1,39 +1,71 @@
-import React, { Component } from "react";
-import { Card, Table } from "react-bootstrap";
-export default class Listtransaction extends Component {
-  render() {
-    return (
-      <div>
-        <Card className={"border border-dark bg-dark text-white"}>
-          <Card.Header> Transaction list</Card.Header>
-          <Card.Body>
-            <Table bordered hover striped variant="dark">
-              <thead>
-                <tr>
-                  <th>Transaction ID</th>
-                  <th>SenderID</th>
-                  <th>senderBIC</th>
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { Card, Table } from "react-bootstrap"
+import { Link } from "react-router-dom";
 
-                  <th>ReceiverNumber</th>
-                  <th>ReceiverName</th>
-                  <th>ReceiverBIC</th>
-                  <th>Amount Transfered</th>
+function Listtransaction() {
+  const [Transaction, setTransaction] = useState([]);
 
-                  <th>TransferDate</th>
-                  <th>Message</th>
-                  <th>TransferType</th>
-                  <th>Actions</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>no transaction avilable</td>
-                </tr>
-              </tbody>
+  useEffect(() => {
+    console.log("running...")
+    axios
+      .get("http://localhost:8080/transaction/")
+      .then((res) => setTransaction(res.data))
+      .catch((err) => console.log("error occured"));
+  }, []);
+
+  function getRowTags() {
+    const rowTags = Transaction.map((t) => (
+      <tr key={t.transactionid}>
+        <td>{t.transactionid}</td>
+        <td>{t.customer.customerid}</td>
+        <td>{t.senderbicBank.bic}</td>
+        <td>{t.receiveraccountholdername}</td>
+        <td>{t.receiveraccountholdernumber}</td>
+        <td>{t.receiverbicBank.bic}</td>
+        <td>{t.amount}</td>
+        <td>{t.transferdate}</td>
+        <td>{t.message.messagecode}</td>
+        <td>{t.transfertypes.transfertypecode}</td>
+        <td key={t.customer.customerid}>
+          <div className="actions">
+            <Link to={"/transaction/" + `${t.transactionid}`}>Show</Link>
+          </div>
+        </td>
+      </tr>
+    ));
+    return rowTags;
+  }
+
+  return (
+      <Card className={"border border-dark bg-dark text-white"}>
+        <Card.Header> Transaction list</Card.Header>
+        <Card.Body>
+          <Table bordered hover striped variant="dark">
+            <thead>
+              <tr>
+                <th>Transaction ID</th>
+                <th>SenderID</th>
+                <th>senderBIC</th>
+
+                <th>ReceiverNumber</th>
+                <th>ReceiverName</th>
+                <th>ReceiverBIC</th>
+                <th>Amount Transfered</th>
+
+                <th>TransferDate</th>
+                <th>Message</th>
+                <th>TransferType</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {getRowTags()}
+            </tbody>
             </Table>
           </Card.Body>
         </Card>
-      </div>
-    );
-  }
+      );
 }
+
+export default Listtransaction;
