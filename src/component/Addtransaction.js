4794e3d5d6fae0aca953e1073940b571b8013258
Hameed <Hameed@gmail.com>
2022-08-10 11:44:53 +0000

    commit

--- src/component/Addtransaction.js
+++ src/component/Addtransaction.js
@@ -1,29 +1,42 @@
-import React, { Component } from 'react'
+import React, { Component , useEffect} from 'react'
 import Navigationbar from './Navigationbar'
+import axios from 'axios';
+
 export default class Addtransaction extends Component {
+
   constructor(props) {
     super(props)
     this.state = {
-        receiverAccountHolderName: "",
-        receiverAccountHolderNumber: "",
-        amount: "",
-        messageCode: "",
-        transfertypeCode:"",
-        currencyCode:"",
-        senderBIC:"",
-        receiverBIC:"",
-        customerId:"",
+      receiveraccountholdernumber: "",
+      receiveraccountholdername: "",
+      amount: "",
+      transferdate: "2022-08-10T11:35:14.396Z",
+        message: {
+        messagecode: "",
+        },
+      transfertypes: {
+        transfertypecode: ""
+      },
+      senderbicBank: {
+        bic: ""
+      },
+      receiverbicBank: {
+        bic: "",
+      },
+      customer: {
+        customerid: ""
+      }
     }
     this.handleSubmit=this.handleSubmit.bind(this)
 }
   rnamehandler = (event) => {
     this.setState({
-        receiverAccountHolderName: event.target.value
+        receiveraccountholdername: event.target.value
     })
 }
 rnumberhandler = (event) => {
     this.setState({
-      receiverAccountHolderNumber  : event.target.value
+      receiveraccountholdernumber  : event.target.value
     })
 }
 amounthandler = (event) => {
@@ -33,48 +46,78 @@ amounthandler = (event) => {
 }
  messageCodehandler = (event) => {
     this.setState({
-       messageCode: event.target.value
+      message: {
+        messagecode: event.target.value
+        }
+      //  messagecode: event.target.value
     })
 }
 transferTypeCodehandler = (event) => {
     this.setState({
-      transfertypeCode: event.target.value
+      transfertypes: {
+        transfertypecode: event.target.value
+      }
+      // transfertypeCode: event.target.value
     })
 }
-currencyCodehandler = (event) => {
-    this.setState({
-       currencyCode: event.target.value
-    })
-}
+// currencyCodehandler = (event) => {
+//     this.setState({
+//       receiverbicBank: {
+//         bic: event.target.value
+//       }
+//       //  currencyCode: event.target.value
+//     })
+// }
    senderBIChandler = (event) => {
     this.setState({
-      senderBIC: event.target.value
+      senderbicBank: {
+        bic: event.target.value
+      }
+      // senderBIC: event.target.value
     })
 }
  receiverBIChandler = (event) => {
     this.setState({
-       receiverBIC: event.target.value
+      receiverbicBank: {
+        bic: event.target.value
+      }
+      //  receiverBIC: event.target.value
     })
 }
 customerIdhandler = (event) => {
     this.setState({
-       customerId: event.target.value
+      customer: {
+        customerid: event.target.value
+      }
+      //  customerId: event.target.value
     })
 }
 handleSubmit = (event) => {
    
    console.log(this.state);
-    this.setState({
-        receiverAccountHolderName: "",
-        receiverAccountHolderNumber: "",
-        amount: "",
-        messageCode: "",
-        transferTypeCode:"",
-        currencyCode: "",
-        senderBIC: "",
-        receiverBIC: '',
-       customerId : "",
-    })
+   axios.post("http://localhost:8080/transaction/", this.state)
+
+
+    // this.setState({
+    //   receiveraccountholdernumber: "",
+    //   receiveraccountholdername: "",
+    //   amount: "",
+    //     message: {
+    //     messagecode: "",
+    //     },
+    //   transfertypes: {
+    //     transfertypecode: ""
+    //   },
+    //   senderbicBank: {
+    //     bic: ""
+    //   },
+    //   receiverbicBank: {
+    //     bic: "",
+    //   },
+    //   customer: {
+    //     customerid: ""
+    //   }
+    // })
  event.preventDefault()
     
 }
@@ -88,28 +131,28 @@ handleSubmit = (event) => {
                     <label>Sender   BIC :</label> <input type="text" value={this.state.senderBIC} onChange={this.senderBIChandler} placeholder="Sender BIC..." /><br />
                     <label>Customer ID:</label> <input type="text" value={this.state.customerId} onChange={this.customerIdhandler} placeholder="CustomerId..." /><br />
                     
-                    <label>Receiver Account Holder Name:</label> <input type="text" value={this.state.receiverAccountHolderName} onChange={this.rnamehandler} placeholder="Receiver Account Holder Name..." /><br />
-                    <label>Receiver Account Holder Number :</label> <input type="text" value={this.state.receiverAccountHolderNumber} onChange={this.rnumberhandler} placeholder="Receiver Account Holder Number..." /><br />
+                    <label>Receiver Account Holder Name:</label> <input type="text" value={this.state.receiveraccountholdername} onChange={this.rnamehandler} placeholder="Receiver Account Holder Name..." /><br />
+                    <label>Receiver Account Holder Number :</label> <input type="text" value={this.state.receiveraccountholdernumber} onChange={this.rnumberhandler} placeholder="Receiver Account Holder Number..." /><br />
                     <label>Receiver BIC :</label> <input type="text" value={this.state.receiverBIC} onChange={this.receiverBIChandler} placeholder="Receiver BIC..." /><br />
 
                     <label>Amount :</label> <input type="text" value={this.state.amount} onChange={this.amounthandler} placeholder="Amount..." /><br />
                     <label>Message Code :</label><select onChange={this.messageCodehandler} defaultValue="Select MessageCode">
                         <option defaultValue>Select MessageCode</option>
-                        <option value="chqb">CHQB</option>
-                        <option value="cort">CORT</option>
-                        <option value="hold">HOLD</option>
-                        <option value="intc">INTC</option>
-                        <option value="phob">PHOB</option>
-                        <option value="phoi">PHOI</option>
-                        <option value="phon">PHON</option>
-                        <option value="repa">REPA</option>
-                        <option value="sdva">SDVA</option>
+                        <option value="CHQB">CHQB</option>
+                        <option value="CORT">CORT</option>
+                        <option value="HOLD">HOLD</option>
+                        <option value="INTC">INTC</option>
+                        <option value="PHOB">PHOB</option>
+                        <option value="PHOI">PHOI</option>
+                        <option value="PHON">PHON</option>
+                        <option value="REPA">REPA</option>
+                        <option value="SDVA">SDVA</option>
                     </select><br />
                    <label>Transfer Type Code :</label><select onChange={this.transferTypeCodehandler} defaultValue="Select Transfer Type Code">
                         <option defaultValue>Select Transfer Type Code</option>
-                        <option value="customertransfer">Customer Transfer</option>
-                        <option value="banktransfer">Bank Transfer</option>
-                        <option value="banktransferforown">Bank Transfer for Own</option>
+                        <option value="Customer Transfer">Customer Transfer</option>
+                        <option value="Bank Transfer">Bank Transfer</option>
+                        <option value="Bank Transfer for Own Account">Bank Transfer for Own Account</option>
                     </select><br />
                      {/* <label>Currency Code :</label><select onChange={this.currencyCodehandler} defaultValue="Select Currency Code">
                         <option defaultValue>Select Currency Code</option>
